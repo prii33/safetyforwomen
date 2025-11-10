628cd00e5530ce40e815069e15c0d7605c53c140
prii33 <prii33@gmail.com>
2025-11-10 21:46:42 +0530

    fix: Correct fluid cursor import path

--- components/FluidCursor.tsx
+++ components/FluidCursor.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect } from 'react';
 
-import useFluidCursor from '../hooks/use-FluidCursor';
+import useFluidCursor from '../hooks/use-fluidCursor';
 
 const FluidCursor = () => {
   useEffect(() => {
